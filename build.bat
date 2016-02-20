746ebd1f8a6dbe2de99ca0e12b0057ac4b0906e5
Ian Bebbington <github@bebbs.co.uk>
2016-02-20 09:43:49 +0000

    Updated Nuget Made build.bat restore packages

--- build.bat
+++ build.bat
@@ -1,5 +1,9 @@
 @echo Off
 
+echo BUILD.BAT - NuGet package restore started.
+".\.nuget\NuGet.exe" restore ".\OneCog.Io.Owl.Intuition.sln" -OutputDirectory ".\packages"
+echo BUILD.BAT - NuGet package restore finished.
+
 echo BUILD.BAT - FAKE build started.
 ".\packages\FAKE.4.20.0\tools\Fake.exe" build.fsx
 echo BUILD.BAT - FAKE build finished.
